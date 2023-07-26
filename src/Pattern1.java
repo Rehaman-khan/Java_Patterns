73a38ec70c5bfb598a9da9756d34514ea6537ac7
Rehaman-khan github <rehamankhan1603@gmail.comgit>
2023-07-26 17:45:24 +0530

    JAVA PATTERNS PROBLEMS

---
+++ src/Pattern1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        int n,m;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Value of n : ");
+        n= sc.nextInt();
+        System.out.println("Enter the value of m :");
+        m= sc.nextInt();
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=m;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
---
+++ src/Pattern4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Pattern4 {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n");
+        n = sc.nextInt();
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
